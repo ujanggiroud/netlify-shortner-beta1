2c9f08fcc378553e09cc84c4ed28dfd287170677
ujanggiroud <89955753+ujanggiroud@users.noreply.github.com>
2026-07-27 22:33:42 +0700

    Update shorten.js
    update aturan masking

--- netlify/functions/shorten.js
+++ netlify/functions/shorten.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   if (event.httpMethod !== "POST") return { statusCode: 405, body: "Method Not Allowed" };
 
-  const { code, url, tag, pixelId } = JSON.parse(event.body);
+  const { code, url, tag, pixelId, image, title } = JSON.parse(event.body);
   const redisUrl = process.env.UPSTASH_REDIS_REST_URL;
   const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN;
 
@@ -9,16 +9,21 @@ exports.handler = async (event) => {
     return { statusCode: 400, body: JSON.stringify({ error: "Code dan URL Wajib Diisi!" }) };
   }
 
-  const payload = JSON.stringify({ url, tag: tag || 'Umum', pixelId: pixelId || '', createdAt: new Date().toISOString() });
+  const payload = JSON.stringify({
+    url,
+    tag: tag || 'Umum',
+    pixelId: pixelId || '',
+    image: image || '',
+    title: title || '',
+    createdAt: new Date().toISOString()
+  });
 
-  // Simpan ke Redis
   await fetch(`${redisUrl}/set/link:${code}`, {
     method: 'POST',
     headers: { Authorization: `Bearer ${redisToken}` },
     body: payload
   });
 
-  // Simpan kode ke daftar list link
   await fetch(`${redisUrl}/sadd/all_links`, {
     method: 'POST',
     headers: { Authorization: `Bearer ${redisToken}` },
